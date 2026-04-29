840767842c8524a84b3a65ac08eab355036a618b
EmnaDorai <emnadorii81@gmail.com>
2026-04-29 21:05:39 +0200

    new877

--- BackEnd/Microservices/Skill/src/main/java/esprit/skill/SkillApplication.java
+++ BackEnd/Microservices/Skill/src/main/java/esprit/skill/SkillApplication.java
@@ -3,22 +3,10 @@ package esprit.skill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-@EnableDiscoveryClient
-
-=======
->>>>>>> b7e93fa9abcd913d3ba37913b8481d5dd480ed43
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
-<<<<<<< HEAD
-=======
->>>>>>> 8d5250d (Ajout du projet MatchFreelance)
->>>>>>> b7e93fa9abcd913d3ba37913b8481d5dd480ed43
 @SpringBootApplication
 public class SkillApplication {
 
