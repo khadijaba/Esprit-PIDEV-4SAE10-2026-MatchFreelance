196ada87b199a052bf99e4de4948936ef12fe147
EmnaDorai <emnadorii81@gmail.com>
2026-04-30 14:43:28 +0200

    new880

--- BackEnd/Microservices/Project/src/main/java/esprit/project/entities/ProjectStatus.java
+++ BackEnd/Microservices/Project/src/main/java/esprit/project/entities/ProjectStatus.java
@@ -1,16 +1,8 @@
 package esprit.project.entities;
 
 public enum ProjectStatus {
-<<<<<<< HEAD
     /** Brouillon : visible uniquement par le project owner jusqu’à publication (OPEN). */
     DRAFT,
-=======
-<<<<<<< HEAD
-=======
-    /** Brouillon : visible uniquement par le project owner jusqu’à publication (OPEN). */
-    DRAFT,
->>>>>>> 8d5250d (Ajout du projet MatchFreelance)
->>>>>>> b7e93fa9abcd913d3ba37913b8481d5dd480ed43
     OPEN,
     IN_PROGRESS,
     COMPLETED,
